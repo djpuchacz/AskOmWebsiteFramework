ebf16eb46f8875f2ce8b22a927ab89931f8d81b1
Lukasz Wozniak <lukaszwozniak@outlook.com>
2023-02-27 14:57:05 +0100

    create reusable method to handle overlays

--- src/test/java/org/selenium/pom/base/BasePage.java
+++ src/test/java/org/selenium/pom/base/BasePage.java
@@ -1,6 +1,13 @@
 package org.selenium.pom.base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class BasePage { //for common objects
     public WebDriver driver;
@@ -11,5 +18,17 @@ public class BasePage { //for common objects
 
     public void load(String endPoint) {
         driver.get("https://askomdch.com" + endPoint);
+    }
+
+    public void waitForOverlaysToDisappear(By overlay){
+        List<WebElement> overlays = driver.findElements(overlay);
+        System.out.println("OVERLAY SIZE: " + overlays.size());
+        if (overlays.size() > 0) {
+            new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.invisibilityOfAllElements(overlays));
+            System.out.println("OVERLAYS ARE INVISIBLE");
+        }
+        else{
+            System.out.println("OVERLAYS NOT FOUND");
+        }
     }
 }
