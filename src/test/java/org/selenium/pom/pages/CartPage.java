cfbfca7b2369226043ded5f95427592db69a68b0
Lukasz Wozniak <lukaszwozniak@outlook.com>
2023-03-01 16:30:35 +0100

    refactoring Cart Page with Page Factory concept

--- src/test/java/org/selenium/pom/pages/CartPage.java
+++ src/test/java/org/selenium/pom/pages/CartPage.java
@@ -2,16 +2,22 @@ package org.selenium.pom.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.selenium.pom.base.BasePage;
 
 public class CartPage extends BasePage {
-    private final By productName = By.cssSelector("td[class='product-name'] a");
+   /* private final By productName = By.cssSelector("td[class='product-name'] a");
     private final By checkoutBtn = By.cssSelector(".checkout-button");
-    private  final By cartHeading = By.cssSelector(".has-text-align-center");
-
+    private  final By cartHeading = By.cssSelector(".has-text-align-center");*/
+    @FindBy(css = "td[class='product-name'] a") private WebElement productName;
+    @FindBy(how = How.CSS, using = ".checkout-button") private WebElement checkoutBtn;
     public CartPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);//96
     }
 
     /*public Boolean isLoaded(){//91
@@ -19,7 +25,8 @@ public class CartPage extends BasePage {
     }*/
 
    public String getProductName(){
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(productName)).getText();
+        //return wait.until(ExpectedConditions.visibilityOfElementLocated(productName)).getText();
+        return wait.until(ExpectedConditions.visibilityOf(productName)).getText();//96
    }
 
    public CheckoutPage checkout(){
