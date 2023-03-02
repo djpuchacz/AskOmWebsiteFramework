03c080ae48b13c0061757a4d2f8fe2fd951d0314
Lukasz Wozniak <lukaszwozniak@outlook.com>
2023-03-02 14:38:35 +0100

    selenium update to web driver auto management

--- src/test/java/org/selenium/pom/factory/DriverManager.java
+++ src/test/java/org/selenium/pom/factory/DriverManager.java
@@ -2,15 +2,14 @@ package org.selenium.pom.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.Duration;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverInfo;
 
 public class DriverManager {
-
         public WebDriver initializeDriver() {
             WebDriver driver = new ChromeDriver();
+            //WebDriver driver = new FirefoxDriver();
             driver.manage().window().maximize();
-            //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
             return driver;
         }
 }
