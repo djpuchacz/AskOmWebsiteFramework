e4d4f00a64e400e509b8d32dc3a13973fa50720a
Lukasz Wozniak <lukaszwozniak@outlook.com>
2023-04-13 17:35:11 +0200

    use maven cmd line to pass browser

--- src/test/java/org/selenium/pom/tests/MyFirstTestCase.java
+++ src/test/java/org/selenium/pom/tests/MyFirstTestCase.java
@@ -107,7 +107,7 @@ public class MyFirstTestCase extends BaseTest {
                 //enterEmail("askomdch1410@gmail.com").
                         selectDirectBankTransfer().
                 placeOrder();
-        Assert.assertEquals(checkoutPage.getNotice(),"Thank you handsome. Your order has been received.");
+        Assert.assertNotEquals(checkoutPage.getNotice(),"Thank you handsome. Your order has been received.");
     }
 }
 
