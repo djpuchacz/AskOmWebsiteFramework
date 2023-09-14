319083cdf5f1ea3c71da5e522b751f3fdc7b82fa
Lukasz Wozniak <lukaszwozniak@outlook.com>
2023-09-14 16:53:27 +0200

    validated AddToCart API

--- src/test/java/org/selenium/pom/api/actions/DummyClass.java
+++ src/test/java/org/selenium/pom/api/actions/DummyClass.java
@@ -9,15 +9,29 @@ public class DummyClass {
         //System.out.println(new SignUpApi().fetchRegisterNonceValueUsingGroovy()); //152
         //System.out.println(new SignUpApi().fetchRegisterNonceValueUsingJsoup());
 
-        String userName = "demouser" + new FakerUtils().generateRandomNumber();
+        /*String userName = "demouser" + new FakerUtils().generateRandomNumber(); //157
         User user = new User().
                 setUsername(userName).
                 setPassword("demopwd").
                 setEmail(userName + "@askomdch.com");
         SignUpApi signUpApi = new SignUpApi();
         System.out.println(signUpApi.register(user));
-        System.out.println(signUpApi.getCookies());
+        System.out.println(signUpApi.getCookies());*/
 
-
+        /*CartApi cartApi = new CartApi(); // 157 user is not log in
+        cartApi.addToCart(1215, 1);
+        System.out.println(cartApi.getCookies());
+*/
+        String userName = "demouser" + new FakerUtils().generateRandomNumber(); //157 user is log in
+        User user = new User().
+                setUsername(userName).
+                setPassword("demopwd").
+                setEmail(userName + "@askomdch.com");
+        SignUpApi signUpApi = new SignUpApi();
+        signUpApi.register(user);
+        System.out.println("REGISTER COOKIES: " + signUpApi.getCookies());
+        CartApi cartApi = new CartApi(signUpApi.getCookies()); // 157 user is log in
+        cartApi.addToCart(1215, 5);
+        System.out.println("CART COOKIES: " + cartApi.getCookies());
     }
 }
