83637069bcb5e20487b00b24983f3370ace095eb
Lukasz Wozniak <lukaszwozniak@outlook.com>
2023-09-24 17:20:30 +0200

    API refactored

--- src/test/java/org/selenium/pom/api/actions/CartApi.java
+++ src/test/java/org/selenium/pom/api/actions/CartApi.java
@@ -4,6 +4,8 @@ import io.restassured.http.Cookies;
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
 import io.restassured.response.Response;
+import org.selenium.pom.api.ApiRequest;
+import org.selenium.pom.constants.EndPoint;
 import org.selenium.pom.utils.ConfigLoader;
 
 import java.util.HashMap;
@@ -36,18 +38,7 @@ public class CartApi { //156
             cookies = new Cookies();
         }
 
-        Response response = given().
-                baseUri(ConfigLoader.getInstance().getBaseUrl()).
-                headers(headers).
-                formParams(formParams).
-                cookies(cookies).
-                log().all().
-        when().
-                post("/?wc-ajax=add_to_cart").
-        then().
-                log().all().
-                extract().
-                response();
+        Response response = ApiRequest.post(EndPoint.ADD_TO_CART.url, headers, formParams, cookies);
         if (response.getStatusCode() != 200) {
             throw
                     new RuntimeException("Failed to add product" + productId + " to the cart" + ", HTTP Status Code: " + response.getStatusCode());
