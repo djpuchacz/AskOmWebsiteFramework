ca81c0176a7c2ef56fa07a588f1aa768a6d12399
Lukasz Wozniak <lukaszwozniak@outlook.com>
2023-09-24 15:44:47 +0200

    created login andCheckoutUsingCashOnDeliveryTest

--- src/test/java/org/selenium/pom/tests/CheckoutTest.java
+++ src/test/java/org/selenium/pom/tests/CheckoutTest.java
@@ -73,4 +73,29 @@ public class CheckoutTest extends BaseTest {
 
         Assert.assertEquals(checkoutPage.getNotice(), "Thank you. Your order has been received.");
     }
+    @Test
+    public void loginAndCheckoutUsingCashOnDelivery() throws IOException {
+        BillingAddress billingAddress = JacksonUtils.deserializeJson("myBillingAddress.json", BillingAddress.class);
+        String userName = "demouser" + new FakerUtils().generateRandomNumber(); //user account create
+        User user = new User().
+                setUsername(userName).
+                setPassword("demopwd").
+                setEmail(userName + "@askomdch.com");
+
+        SignUpApi signUpApi = new SignUpApi(); //register of above user
+        signUpApi.register(user);
+
+        CartApi cartApi = new CartApi(signUpApi.getCookies()); //add product to a card as a guest
+        Product product = new Product(1215);
+        cartApi.addToCart(product.getId(), 3);
+
+        CheckoutPage checkoutPage = new CheckoutPage(getDriver()).load();
+        injectCookiesToBrowser(signUpApi.getCookies()); //cookies injecting in order to log in
+        checkoutPage.load().
+                setBillingAddress(billingAddress).
+                selectCashOnDelivery().
+                placeOrder();
+
+        Assert.assertEquals(checkoutPage.getNotice(), "Thank you. Your order has been received.");
+    }
 }
